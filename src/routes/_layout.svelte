db9dbc470b6636d94a214f51249e1461a5b29267
wildpow <element1028@gmail.com>
2020-03-28 14:35:17 -0700

    Working with a different config

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -4,17 +4,11 @@
 	export let segment;
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+<style global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 </style>
-
 <Nav {segment}/>
 
 <main>
